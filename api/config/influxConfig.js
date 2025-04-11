0a46d20038cf14ba7c40e4110d475c518023f8c0
Apsharan <94956246+Apsharan@users.noreply.github.com>
2025-04-11 11:40:53 +0200

    Delete api directory

--- api/config/influxConfig.js
+++
@@ -1,8 +0,0 @@
-const influxConfig = {
-    url: 'http://influxdb:8086',
-    token: 'mysecrettoken',
-    org: 'water_org',  // ✅ Use org NAME, not ID
-    bucket: 'water_bucket'
-};
-
-module.exports = influxConfig;
